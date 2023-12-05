e0fa275533bdd1c7714ebae1ccfee85258cd6586
Friedrich Tschirpke <friedrich.tschirpke@student.hu-berlin.de>
2023-12-05 09:49:42 +0100

    2023: day05 created

---
+++ 2023/day05/src/lib.rs
@@ -0,0 +1,29 @@
+pub mod part1;
+pub mod part2;
+
+#[cfg(test)]
+mod tests {
+    use crate::part1;
+    use crate::part2;
+    use aoclib::AocError;
+
+    #[test]
+    fn test_part1() -> Result<(), AocError> {
+        let input = include_str!("../test_input1.txt");
+        let output = part1::process(input)?;
+        let expected_output = -1;
+        todo!("expected output part 1");
+        assert_eq!(expected_output, output);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part2() -> Result<(), AocError> {
+        let input = include_str!("../test_input2.txt");
+        let output = part2::process(input)?;
+        let expected_output = -1;
+        todo!("expected output part 2");
+        assert_eq!(expected_output, output);
+        Ok(())
+    }
+}
