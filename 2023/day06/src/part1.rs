36f0cea17fd166c6fdcf55f704f40ade0e712241
Friedrich Tschirpke <friedrich.tschirpke@student.hu-berlin.de>
2023-12-06 15:03:23 +0100

    2023: day06 part2

--- 2023/day06/src/part1.rs
+++ 2023/day06/src/part1.rs
@@ -1,22 +1,48 @@
 use aoclib::AocError;
+
 use nom::{
     bytes::complete::tag,
-    character::complete::{newline, space1, u32 as u32_parser},
+    character::complete::{newline, space1, u64 as u64_parser},
     multi::separated_list1,
     sequence::preceded,
 };
 
 pub struct Record {
-    pub time: u32,
-    pub distance: u32,
+    pub time: u64,
+    pub distance: u64,
+}
+
+pub fn score_for(race_time: u64, time_held: u64) -> u64 {
+    if time_held <= race_time {
+        (race_time - time_held) * time_held
+    } else {
+        0
+    }
+}
+
+pub fn score_derivative(race_time: u64, time_held: u64) -> u64 {
+    if time_held <= race_time {
+        race_time - 2 * time_held
+    } else {
+        0
+    }
+}
+
+pub fn best_time_held(race_time: u64) -> (u64, Option<u64>) {
+    let half = race_time / 2;
+    if race_time % 2 == 0 {
+        (half, None)
+    } else {
+        (half, Some(half + 1))
+    }
 }
 
 pub fn parse_records(input: &str) -> Result<Vec<Record>, AocError> {
     let (input, _) = preceded(tag("Time:"), space1)(input)?;
-    let (input, times) = separated_list1(space1, u32_parser)(input)?;
+    let (input, times) = separated_list1(space1, u64_parser)(input)?;
     let (input, _) = newline(input)?;
     let (input, _) = preceded(tag("Distance:"), space1)(input)?;
-    let (input, distances) = separated_list1(space1, u32_parser)(input)?;
+    let (input, distances) = separated_list1(space1, u64_parser)(input)?;
     let (rest, _) = newline(input)?;
     if rest.is_empty() {
         if times.len() != distances.len() {
@@ -37,6 +63,17 @@ pub fn parse_records(input: &str) -> Result<Vec<Record>, AocError> {
     }
 }
 
-pub fn process(input: &'static str) -> Result<i32, AocError> {
-    todo!("Part 1");
+pub fn process(input: &'static str) -> Result<u64, AocError> {
+    let records = parse_records(input)?;
+    let product = records.iter().fold(1u64, |acc, record| {
+        let (max_time_held, alternative) = best_time_held(record.time);
+        let mut possible_improvements: u64 = if alternative.is_some() { 2 } else { 1 };
+        let mut time_held = max_time_held - 1;
+        while score_for(record.time, time_held) > record.distance {
+            possible_improvements += 2;
+            time_held -= 1;
+        }
+        possible_improvements * acc
+    });
+    Ok(product)
 }
--- 2023/day06/src/part2.rs
+++ 2023/day06/src/part2.rs
@@ -1,5 +1,55 @@
 use aoclib::AocError;
 
-pub fn process(input: &'static str) -> Result<i32, AocError> {
-    todo!("Part 2");
+use nom::{
+    bytes::complete::tag,
+    character::complete::{digit1, newline, space1},
+    multi::separated_list1,
+    sequence::preceded,
+};
+
+use crate::part1::{best_time_held, Record};
+
+fn parse_record(input: &str) -> Result<Record, AocError> {
+    let (input, _) = preceded(tag("Time:"), space1)(input)?;
+    let (input, time_parts) = separated_list1(space1, digit1)(input)?;
+    let (input, _) = newline(input)?;
+    let (input, _) = preceded(tag("Distance:"), space1)(input)?;
+    let (input, distance_parts) = separated_list1(space1, digit1)(input)?;
+    let (rest, _) = newline(input)?;
+    let time_str = time_parts.into_iter().collect::<String>();
+    dbg!(&time_str);
+    let distance_str = distance_parts.into_iter().collect::<String>();
+    dbg!(&distance_str);
+    let time = time_str
+        .parse::<u64>()
+        .map_err(|_| AocError::ParseError(format!("Failed to parse time: {}", time_str)))?;
+    let distance = distance_str
+        .parse::<u64>()
+        .map_err(|_| AocError::ParseError(format!("Failed to parse distance: {}", distance_str)))?;
+    if rest.is_empty() {
+        Ok(Record { time, distance })
+    } else {
+        Err(AocError::ParseError(format!(
+            "Parsing of time and distance left rest: {}",
+            rest
+        )))
+    }
+}
+
+// Let's calculate the time_held for a given distance
+// (race_time - time_held) * time_held == distance
+// -> time_held^2 - race_time * time_held + distance == 0
+// -> time_held = race_time / 2 +- sqrt( race_time^2 / 4 - distance )
+
+pub fn process(input: &'static str) -> Result<u64, AocError> {
+    let record = parse_record(input)?;
+    let (best_time_held, alternative) = best_time_held(record.time);
+    let number_of_maxima = if alternative.is_some() { 2 } else { 1 };
+    let helper = (record.time as f64).powi(2) / 4.0 - record.distance as f64;
+    let helper = helper.sqrt() as u64;
+    if helper <= 1 {
+        return Ok(number_of_maxima);
+    }
+    let number_of_better_solutions = number_of_maxima + helper * 2;
+    Ok(number_of_better_solutions)
 }
