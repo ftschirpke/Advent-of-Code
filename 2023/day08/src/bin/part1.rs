a48bf4bb28741f7d1307dfca2d5d1fc060f78d8f
Friedrich Tschirpke <friedrich.tschirpke@student.hu-berlin.de>
2023-12-08 12:48:39 +0100

    2023: day08 created

---
+++ 2023/day08/src/bin/part1.rs
@@ -0,0 +1,11 @@
+use aoclib::AocError;
+
+use day08::part1::process;
+
+fn main() -> Result<(), AocError> {
+    let input = include_str!("../../input.txt");
+    let output = process(input)?;
+    println!("=== Part 1 Result ===");
+    println!("{}", output);
+    Ok(())
+}
