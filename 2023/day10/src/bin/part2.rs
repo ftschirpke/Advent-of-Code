7841a8222b5f0b9b076e714dac8fb904a0714b69
Friedrich Tschirpke <friedrich.tschirpke@student.hu-berlin.de>
2023-12-10 12:45:28 +0100

    2023: day10 created

---
+++ 2023/day10/src/bin/part2.rs
@@ -0,0 +1,11 @@
+use aoclib::AocError;
+
+use day10::part2::process;
+
+fn main() -> Result<(), AocError> {
+    let input = include_str!("../../input.txt");
+    let output = process(input)?;
+    println!("=== Part 2 Result ===");
+    println!("{}", output);
+    Ok(())
+}
