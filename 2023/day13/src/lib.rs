06b059f121814034c16bd999b8a575b85bb37626
Friedrich Tschirpke <friedrich.tschirpke@student.hu-berlin.de>
2023-12-13 14:30:08 +0100

    2023: day13 created

---
+++ 2023/day13/src/lib.rs
@@ -0,0 +1,39 @@
+pub mod part1;
+pub mod part2;
+
+#[cfg(test)]
+mod tests {
+    use crate::part1;
+    use crate::part2;
+    use aoclib::AocError;
+
+    #[test]
+    fn test_part1() -> Result<(), AocError> {
+        let input = include_str!("../test_input1.txt");
+        if input.is_empty() {
+            return Err(AocError::ParseError(
+                "Input file for test 1 is empty".to_string(),
+            ));
+        }
+        let output = part1::process(input)?;
+        let expected_output = -1;
+        todo!("expected output part 1");
+        assert_eq!(expected_output, output);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part2() -> Result<(), AocError> {
+        let input = include_str!("../test_input2.txt");
+        if input.is_empty() {
+            return Err(AocError::ParseError(
+                "Input file for test 2 is empty".to_string(),
+            ));
+        }
+        let output = part2::process(input)?;
+        let expected_output = -1;
+        todo!("expected output part 2");
+        assert_eq!(expected_output, output);
+        Ok(())
+    }
+}
